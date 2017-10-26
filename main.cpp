b866fdbbb552241b471fcc9328c553cc817a18cd
munozyanez <munoz.yanez@gmail.com>
2017-10-26 14:54:56 +0200

    modified:   lib/CiA402Device 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 
-using namespace std;
 
 #include <iostream>
 
 
+#include "TestPort.h"
 #include "Cia402device.h"
+#include "CiA301CommPort.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
 
 
-    //CiA402DeviceICanbus dev1;
+    TestPort port;
+    CiA301CommPort coms(port.getPortFileDescriptor());
+
+
+    CiA402Device j1;
+//    j1.SetCommunications(&coms);
 
     cout << od::controlword[1] << "  --> " << sizeof(od::controlword) / sizeof(od::controlword[0]) << endl;
     vector<unsigned char> message();
