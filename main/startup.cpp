19a5738ec0d3a0ccf09120d5e064ca916c372d16
munozyanez <munoz.yanez@gmail.com>
2017-11-01 19:33:48 +0100

    modified:   CMakeLists.txt 	modified:   lib/CiA402Device 	new file:   main/startup.cpp

---
+++ main/startup.cpp
@@ -0,0 +1,35 @@
+
+
+#include <iostream>
+
+
+#include "TestPort.h"
+#include "CanBusPort.h"
+#include "Cia402device.h"
+#include "CiA301CommPort.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+
+    ///prepare ports
+    /// Open a port address with a PortBase Object
+    TestPort port;
+    //CanBusPort port;
+
+    ///Then instantiate a 301 communications object for that address
+    CiA301CommPort coms(port.getPortFileDescriptor());
+
+    ///Create a joint and give a canopen id, and a 301port (by constructor)
+    CiA402Device j1(2,&coms);
+
+    ///Check the status of the device
+    j1.CheckStatus();
+    //j1.Start();
+
+
+
+    return 0;
+}
