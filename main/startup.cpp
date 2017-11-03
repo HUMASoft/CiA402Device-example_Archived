4ee59f1ba73d35a5ebfbc350e8bf4c874f6d6853
teo <teo@pruebas.uc3m.es>
2017-11-03 21:11:05 +0100

    modified:   lib/CiA402Device 	modified:   main/startup.cpp

--- main/startup.cpp
+++ main/startup.cpp
@@ -16,19 +16,19 @@ int main(int argc, char *argv[])
 
     ///prepare ports
     /// Open a port address with a PortBase Object
-    TestPort port;
-    //CanBusPort port;
+    //TestPort port;
+    CanBusPort port;
 
     ///Then instantiate a 301 communications object for that address
     //CiA301CommPort coms(port.getPortFileDescriptor());
 
     ///Create a joint and give a canopen id, and a 301port (by constructor)
-    CiA402Device j1(2,port.getPortFileDescriptor());
+    CiA402Device j1(1,port.getPortFileDescriptor());
 
     ///Check the status of the device
+    //j1.CheckStatus();
+    j1.SwitchOn();
     j1.CheckStatus();
-    //j1.Start();
-
 
 
     return 0;
