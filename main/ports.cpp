559be401ba8792ee0d2f5aca996578d5453a255a
munozyanez <munoz.yanez@gmail.com>
2017-12-30 13:37:49 +0100

    nuevo archivo:  main/ports.cpp

---
+++ main/ports.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+
+
+#include "TestPort.h"
+#include "CanBusPort.h"
+#include "SocketCanPort.h"
+#include "Cia402device.h"
+#include "CiA301CommPort.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+
+    ///prepare ports
+    /// Open a port address with a PortBase Object
+    //TestPort port;
+    SocketCanPort port("vcan0");
+    std::vector<uint8_t> data={};
+    port.PutMsg(0x123,data.data(),0);
+
+
+
+    return 0;
+}
