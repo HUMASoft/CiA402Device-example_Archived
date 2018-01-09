5447ba0bae9f4e9260f04b6931ee8a2c92b0a06c
teo <teo@pruebas.uc3m.es>
2018-01-09 21:18:56 +0100

    modified:   lib/CiA402Device 	modified:   main/position.cpp 	modified:   main/startup.cpp

--- main/position.cpp
+++ main/position.cpp
@@ -7,6 +7,7 @@
 #include "CanBusPort.h"
 #include "Cia402device.h"
 #include "CiA301CommPort.h"
+#include "SocketCanPort.h"
 
 
 using namespace std;
@@ -17,7 +18,8 @@ int main(int argc, char *argv[])
     ///prepare ports
     /// Open a port address with a PortBase Object
     //TestPort port;
-    CanBusPort port;
+    //CanBusPort port;
+    SocketCanPort p1("can0");
     uint16_t status;
 
 //uint32_t posdegree=180;
@@ -26,7 +28,7 @@ int main(int argc, char *argv[])
     //CiA301CommPort coms(port.getPortFileDescriptor());
 
     ///Create a joint and give a canopen id, and a 301port (by constructor)
-    CiA402Device j1(6,port.getPortFD());
+    CiA402Device j1(6,&p1);
 
     ///Check the status of the device
 //    j1.PrintStatus();
@@ -36,7 +38,7 @@ int main(int argc, char *argv[])
 //   //cout<<posicion<<endl;
   j1.SetupPositionMode(100000,100000);
   sleep(2);
-// j1.SetPosition(DegreeConv(posdegree)); //Needs revsion, take the position in degrees, then converts it and finally sets the position
+ //j1.SetPosition(DegreeConv(posdegree)); //Needs revsion, take the position in degrees, then converts it and finally sets the position
    j1.SetPosition(1000);
    j1.FlushBuffer();
    sleep(2);
--- main/startup.cpp
+++ main/startup.cpp
@@ -4,9 +4,10 @@
 
 
 #include "TestPort.h"
-#include "CanBusPort.h"
+//#include "CanBusPort.h"
 #include "Cia402device.h"
 #include "CiA301CommPort.h"
+#include "SocketCanPort.h"
 
 using namespace std;
 
@@ -17,20 +18,21 @@ int main(int argc, char *argv[])
     ///prepare ports
     /// Open a port address with a PortBase Object
     //TestPort port;
-    CanBusPort port("/dev/can0");//("/dev/can0")
+    //CanBusPort port("/dev/can0");//("/dev/can0")
+    SocketCanPort p1("can0");
     uint16_t status;
 
     ///Then instantiate a 301 communications object for that address
     //CiA301CommPort coms(port.getPortFileDescriptor());
 
     ///Create a joint and give a canopen id, and a 301port (by constructor)
-    CiA402Device j1(6,port.getPortFD());
+    CiA402Device j1(6,&p1);
 
     ///Check the status of the device
     //j1.PrintStatus();
 
     j1.SwitchOn();
-    j1.PrintStatus();
+    //j1.PrintStatus();
 
 
 
